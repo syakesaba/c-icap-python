7fe7ad22bbea8f00799424c79e44a96aa8c6b532
kambayashi.seiya <kambayashi.seiya@jp.panasonic.com>
2023-11-17 10:28:02 +0900

    t

--- src/pyci_module/load_module.c
+++ src/pyci_module/load_module.c
@@ -34,8 +34,8 @@
 #include "pyci_module.h"
 #include "pyci_debug.h"
 
-int append_python_path(const char * service_file);//if ret != 0, ERROR.
-PyObject * import_python_module(const char * service_file);//if NULL, ERROR.
+int append_python_path(const char *service_file);         // if ret != 0, ERROR.
+PyObject *import_python_module(const char *service_file); // if NULL, ERROR.
 
 /*
  * TODO: 一つのpythonファイルmoduleにつき一つの
@@ -64,28 +64,29 @@ PyObject * import_python_module(const char * service_file);//if NULL, ERROR.
  * んでserviceの時は
  */
 
-
 /*
  * service_fileは残念ながらフルパスです。
  */
-ci_service_module_t * load_python_module(const char * service_file){
-    pyci_debug_printf(PYCI_INFO_LEVEL,"starts");
+ci_service_module_t *load_python_module(const char *service_file)
+{
+    pyci_debug_printf(PYCI_INFO_LEVEL, "starts");
     pyci_debug_printf(PYCI_INFO_LEVEL, "loading %s", service_file);
 
-    PyObject * pModule;
-    PyObject * pObj;
-    PyObject * pList;
-    PyObject * pService_Name;
-    PyObject * pService_Descr;
+    PyObject *pModule;
+    PyObject *pObj;
+    PyObject *pList;
+    PyObject *pService_Name;
+    PyObject *pService_Descr;
 
-    ci_service_module_t * service; /* こいつはpythonスクリプト一個につき一個だけ生成される */
+    ci_service_module_t *service; /* こいつはpythonスクリプト一個につき一個だけ生成される */
 
     /* ===================================================
      *  このファイルの核。サービス構造体の作成処理
      * ===================================================
      */
     service = (ci_service_module_t *)malloc(sizeof(ci_service_module_t));
-    if (service == NULL) {
+    if (service == NULL)
+    {
         pyci_debug_printf(PYCI_ERROR_LEVEL, "FAIL at Allocating memory for ci_service_module_t of %s", service_file);
         return NULL;
     }
@@ -94,7 +95,8 @@ ci_service_module_t * load_python_module(const char * service_file){
      *  あらゆるフルパスであってもPythonのパスに突っ込む処理
      * ===================================================
      */
-    if (append_python_path(service_file) != EXIT_SUCCESS){
+    if (append_python_path(service_file) != EXIT_SUCCESS)
+    {
         goto invalid_path_error;
     }
 
@@ -103,7 +105,8 @@ ci_service_module_t * load_python_module(const char * service_file){
      * ===================================================
      */
     pModule = import_python_module(service_file);
-    if (pModule == NULL) {
+    if (pModule == NULL)
+    {
         goto cant_import_error;
     }
 
@@ -112,72 +115,87 @@ ci_service_module_t * load_python_module(const char * service_file){
      * ===================================================
      */
     pObj = PyObject_GetAttrString(pModule, PYCI_CLASS);
-    if (pObj == NULL) {
-        pyci_debug_printf(PYCI_ERROR_LEVEL, "Error at PyObject_GetAttrString(pModule, "PYCI_CLASS")" );
+    if (pObj == NULL)
+    {
+        pyci_debug_printf(PYCI_ERROR_LEVEL, "Error at PyObject_GetAttrString(pModule, " PYCI_CLASS ")");
         goto no_class_error;
     }
 
     /* "ICAP"という名前のクラスをpythonファイルから持ってくる */
-    if (PyCallable_Check(pObj)) {
+    if (PyCallable_Check(pObj))
+    {
         /* pythonではClassをインスタンスにするときも関数を呼び出すときも、Callをする。*/
         Py_INCREF(pModule); /* pClassオブジェクトはpModuleの下にくっついているので、親のpModuleが解放されないようにする */
         pClass = pObj;
         /* グローバル空間にこの雛形を格納!!!!!!!!!!!!!!!!!!!!!!!!!!ここ重要 */
-    } else {
-        pyci_debug_printf(PYCI_ERROR_LEVEL, "FAIL at PyCallable_Check(pObj) : class '"PYCI_CLASS"' is not Callable");
+    }
+    else
+    {
+        pyci_debug_printf(PYCI_ERROR_LEVEL, "FAIL at PyCallable_Check(pObj) : class '" PYCI_CLASS "' is not Callable");
         goto cant_call_class_error;
     }
 
-    //ここまでがPythonファイルからクラスをとってきてグローバル空間に落とし込む処理
-    //ここからがserviceハンドラを保持する構造体の作成
-    //この時点ではpModule(2),pClass(1)という参照数
+    // ここまでがPythonファイルからクラスをとってきてグローバル空間に落とし込む処理
+    // ここからがserviceハンドラを保持する構造体の作成
+    // この時点ではpModule(2),pClass(1)という参照数
 
     /* ===================================================
      *  Pythonスクリプトから、RESPMOD,REQMODの対応状況を見る処理
      * ===================================================
      */
     int mode = 0;
-    pList = PyObject_GetAttrString(pClass, PYCI_CLASS_LIST_SUPPORTED_METHODS);//E+
-    if (pList != NULL && PyList_Check(pList)) {
+    pList = PyObject_GetAttrString(pClass, PYCI_CLASS_LIST_SUPPORTED_METHODS); // E+
+    if (pList != NULL && PyList_Check(pList))
+    {
         int sizeof_pList = -1;
-        sizeof_pList = PyList_Size(pList);//エラー放出するだろうけど、Python公式ドキュメントに書いてないw
-        if (sizeof_pList == -1) {
+        sizeof_pList = PyList_Size(pList); // エラー放出するだろうけど、Python公式ドキュメントに書いてないw
+        if (sizeof_pList == -1)
+        {
             goto pList_size_error;
         }
         int i;
-        char * item;
-        for(i=0;i<sizeof_pList;i++) {
-            PyObject * pItem = PyList_GetItem(pList,i);//借り参照。DECREF禁止。//E+
-            if (pItem != NULL && PyString_Check(pItem)) {
-                //int error = PyString_AsStringAndSize(pItem, item, max);
-                //TODO: ユーザが大量の文字数を指定してきやがった場合↑。本当はこうすべきだが
-                item = PyString_AsString(pItem);//dont dealloc
-                if (item == NULL){
+        char *item;
+        for (i = 0; i < sizeof_pList; i++)
+        {
+            PyObject *pItem = PyList_GetItem(pList, i); // 借り参照。DECREF禁止。//E+
+            if (pItem != NULL && PyString_Check(pItem))
+            {
+                // int error = PyString_AsStringAndSize(pItem, item, max);
+                // TODO: ユーザが大量の文字数を指定してきやがった場合↑。本当はこうすべきだが
+                item = PyString_AsString(pItem); // dont dealloc
+                if (item == NULL)
+                {
                     goto item_not_string_error;
                 }
-//                ci_debug_printf(1,"D0 ret -> %d\n", ret);
-                if (item != NULL) {
-                    //strncmpではない理由は、ヌルキャラの存在により比較失敗するのを恐れたため。
-                    //ここもstrnstrにすべきだが・・・倍返しだ！
-                    if (strstr(item, ci_method_string(ICAP_REQMOD))) {
+                //                ci_debug_printf(1,"D0 ret -> %d\n", ret);
+                if (item != NULL)
+                {
+                    // strncmpではない理由は、ヌルキャラの存在により比較失敗するのを恐れたため。
+                    // ここもstrnstrにすべきだが・・・倍返しだ！
+                    if (strstr(item, ci_method_string(ICAP_REQMOD)))
+                    {
                         mode = mode | ICAP_REQMOD;
                         pyci_debug_printf(PYCI_MESSAGE_LEVEL, "(%s) : REQMOD enabled", service_file);
                     }
-                    if (strstr(item,ci_method_string(ICAP_RESPMOD))) {
+                    if (strstr(item, ci_method_string(ICAP_RESPMOD)))
+                    {
                         mode = mode | ICAP_RESPMOD;
                         pyci_debug_printf(PYCI_MESSAGE_LEVEL, "(%s) : RESPMOD enabled", service_file);
                     }
                 }
-            } else {
+            }
+            else
+            {
                 pyci_debug_printf(PYCI_ERROR_LEVEL,
-                        "(%s) invalid value in "PYCI_CLASS"."PYCI_CLASS_LIST_SUPPORTED_METHODS"[%d]",
-                        service_file,i);
+                                  "(%s) invalid value in " PYCI_CLASS "." PYCI_CLASS_LIST_SUPPORTED_METHODS "[%d]",
+                                  service_file, i);
                 goto invalid_value_in_supported_list_error;
             }
         }
     }
-    if (mode == 0) {
-        pyci_debug_printf(PYCI_ERROR_LEVEL, "FAIL at mode selection for %s : see "PYCI_CLASS"."PYCI_CLASS_LIST_SUPPORTED_METHODS, service_file);
+    if (mode == 0)
+    {
+        pyci_debug_printf(PYCI_ERROR_LEVEL, "FAIL at mode selection for %s : see " PYCI_CLASS "." PYCI_CLASS_LIST_SUPPORTED_METHODS, service_file);
         goto no_mode_declared_error;
     }
 
@@ -185,37 +203,44 @@ ci_service_module_t * load_python_module(const char * service_file){
      *  Pythonスクリプトから、サービス名を取得する処理
      * ===================================================
      */
-    char * service_name;
-    pService_Name = PyObject_GetAttrString(pClass, PYCI_CLASS_STRING_SERVICE_NAME);//E+ N
-    if (pService_Name != NULL && PyString_Check(pService_Name)) {
-        //int error = PyString_AsStringAndSize
-        //TODO: ユーザが大量の文字数を指定してきやがった場合↑。本当はこうすべきだが
-        service_name = PyString_AsString(pService_Name);//dont dealloc
-        if (service_name == NULL) {
+    char *service_name;
+    pService_Name = PyObject_GetAttrString(pClass, PYCI_CLASS_STRING_SERVICE_NAME); // E+ N
+    if (pService_Name != NULL && PyString_Check(pService_Name))
+    {
+        // int error = PyString_AsStringAndSize
+        // TODO: ユーザが大量の文字数を指定してきやがった場合↑。本当はこうすべきだが
+        service_name = PyString_AsString(pService_Name); // dont dealloc
+        if (service_name == NULL)
+        {
             goto service_name_not_string_error;
         }
         pyci_debug_printf(PYCI_MESSAGE_LEVEL, "(%s) : SERVICE_NAME is %s", service_file, service_name);
-    } else {
+    }
+    else
+    {
         goto invalid_service_name_error;
     }
-
 
     /* ===================================================
      *  Pythonスクリプトから、サービスの概要を取得する処理
      *  (サービス名よりもサービスの概要が優先されて表示されるので注意）
      * ===================================================
      */
-    char * service_descr;
-    pService_Descr = PyObject_GetAttrString(pClass, PYCI_CLASS_STRING_SERVICE_DESCRIPTION);//E+ N
-    if (pService_Descr != NULL && PyString_Check(pService_Descr)) {
-        //int error = PyString_AsStringAndSize
-        //TODO: ユーザが大量の文字数を指定してきやがった場合↑。本当はこうすべきだが
-        service_descr = PyString_AsString(pService_Descr);//dont dealloc
-        if (service_descr == NULL) {
+    char *service_descr;
+    pService_Descr = PyObject_GetAttrString(pClass, PYCI_CLASS_STRING_SERVICE_DESCRIPTION); // E+ N
+    if (pService_Descr != NULL && PyString_Check(pService_Descr))
+    {
+        // int error = PyString_AsStringAndSize
+        // TODO: ユーザが大量の文字数を指定してきやがった場合↑。本当はこうすべきだが
+        service_descr = PyString_AsString(pService_Descr); // dont dealloc
+        if (service_descr == NULL)
+        {
             goto service_descr_not_string_error;
         }
         pyci_debug_printf(PYCI_MESSAGE_LEVEL, "(%s) : SERVICE_DESCRIPTION is %s", service_file, service_descr);
-    } else {
+    }
+    else
+    {
         goto invalid_service_descr_error;
     }
 
@@ -223,8 +248,8 @@ ci_service_module_t * load_python_module(const char * service_file){
      *  service構造体の完成
      * ===================================================
      */
-    service->mod_data = NULL; //常にNULL
-    service->mod_conf_table = NULL;//c-icap.confのデータが入ったci_conf_entry構造体が入るらしい。設定はいじりません
+    service->mod_data = NULL;       // 常にNULL
+    service->mod_conf_table = NULL; // c-icap.confのデータが入ったci_conf_entry構造体が入るらしい。設定はいじりません
     service->mod_init_service = python_init_service;
     service->mod_post_init_service = python_post_init_service;
     service->mod_close_service = python_close_service;
@@ -238,27 +263,28 @@ ci_service_module_t * load_python_module(const char * service_file){
     service->mod_type = mode;
 
     pyci_debug_printf(PYCI_MESSAGE_LEVEL,
-            "(%s) : OK! service loaded", service_file);
-    return service;//サービスを返す
+                      "(%s) : OK! service loaded", service_file);
+    return service; // サービスを返す
 
-    /* ===================================================
-     *  一度でもエラーが起きたらここへ飛び、PyObject*の後始末、PyErrの表示をさせる
-     * ===================================================
-     */
-    invalid_path_error:
-    cant_import_error:
-    no_class_error:
-    cant_call_class_error:
-    pList_size_error:
-    invalid_value_in_supported_list_error:
-    item_not_string_error:
-    no_mode_declared_error:
-    service_name_not_string_error:
-    invalid_service_name_error:
-    service_descr_not_string_error:
-    invalid_service_descr_error:
+/* ===================================================
+ *  一度でもエラーが起きたらここへ飛び、PyObject*の後始末、PyErrの表示をさせる
+ * ===================================================
+ */
+invalid_path_error:
+cant_import_error:
+no_class_error:
+cant_call_class_error:
+pList_size_error:
+invalid_value_in_supported_list_error:
+item_not_string_error:
+no_mode_declared_error:
+service_name_not_string_error:
+invalid_service_name_error:
+service_descr_not_string_error:
+invalid_service_descr_error:
 
-    if (PyErr_Occurred()) {
+    if (PyErr_Occurred())
+    {
         PyErr_Print();
     }
     Py_XDECREF(pService_Descr);
@@ -273,31 +299,36 @@ ci_service_module_t * load_python_module(const char * service_file){
 #define PYLIST_APPEND_SUCCESS 0
 #define PYSYS_SET_OBJECT_SUCCESS 0
 
-int append_python_path(const char * service_file) {
+int append_python_path(const char *service_file)
+{
     int ret = 0;
-    char * cp_service_file;
-    const char * dir_name;
+    char *cp_service_file;
+    const char *dir_name;
 
-    cp_service_file = strndup(service_file,strlen(service_file));//returns copied-allocated one.
-    dir_name = dirname(cp_service_file);//returns pointer. not allocted one.
-    PyObject * pName = PyString_FromString(dir_name);//Don't DECREF(pName)
+    cp_service_file = strndup(service_file, strlen(service_file)); // returns copied-allocated one.
+    dir_name = dirname(cp_service_file);                           // returns pointer. not allocted one.
+    PyObject *pName = PyString_FromString(dir_name);               // Don't DECREF(pName)
     free(cp_service_file);
-    if (pName == NULL) {
-        pyci_debug_printf(PYCI_ERROR_LEVEL,"Error in PyString_FromString() of %s", service_file);
+    if (pName == NULL)
+    {
+        pyci_debug_printf(PYCI_ERROR_LEVEL, "Error in PyString_FromString() of %s", service_file);
         return -1;
     }
-    PyObject * pPath = PySys_GetObject(PYTHONPATH_LOCATION_IN_SYS);//NULLきてもErr無し！
-    if (pPath == NULL) {
-        pyci_debug_printf(PYCI_ERROR_LEVEL,"Error in PySys_GetObject("PYTHONPATH_LOCATION_IN_SYS") of %s", service_file);
+    PyObject *pPath = PySys_GetObject(PYTHONPATH_LOCATION_IN_SYS); // NULLきてもErr無し！
+    if (pPath == NULL)
+    {
+        pyci_debug_printf(PYCI_ERROR_LEVEL, "Error in PySys_GetObject(" PYTHONPATH_LOCATION_IN_SYS ") of %s", service_file);
         return -1;
     }
-    ret = PyList_Append(pPath,pName);//E+
-    if (ret != PYLIST_APPEND_SUCCESS) {
-        pyci_debug_printf(PYCI_ERROR_LEVEL,"Error in PyList_Append(pPath,pName) of %s", service_file);
+    ret = PyList_Append(pPath, pName); // E+
+    if (ret != PYLIST_APPEND_SUCCESS)
+    {
+        pyci_debug_printf(PYCI_ERROR_LEVEL, "Error in PyList_Append(pPath,pName) of %s", service_file);
         return ret;
     }
-    ret = PySys_SetObject(PYTHONPATH_LOCATION_IN_SYS,pPath);
-    if (ret != PYSYS_SET_OBJECT_SUCCESS) {
+    ret = PySys_SetObject(PYTHONPATH_LOCATION_IN_SYS, pPath);
+    if (ret != PYSYS_SET_OBJECT_SUCCESS)
+    {
         return ret;
     }
     return ret;
@@ -310,18 +341,21 @@ int append_python_path(const char * service_file) {
  * on Error = NULL (python error may raise)
  * on Success = pModule
  */
-PyObject * import_python_module(const char * service_file) {
-    char * cp_service_file = strndup(service_file,strlen(service_file));
-    if (cp_service_file == NULL) {
-        pyci_debug_printf(PYCI_ERROR_LEVEL,"Could not allocating memory for %s", service_file);
+PyObject *import_python_module(const char *service_file)
+{
+    char *cp_service_file = strndup(service_file, strlen(service_file));
+    if (cp_service_file == NULL)
+    {
+        pyci_debug_printf(PYCI_ERROR_LEVEL, "Could not allocating memory for %s", service_file);
         return NULL;
     }
-    char * service_file_name = (char *)basename(cp_service_file);
-    service_file_name[strlen(service_file_name)-3] = '\0'; // -3 = strlen(".py") equal to basename $1 .py
-    PyObject * pModule = PyImport_ImportModule(service_file_name);//NewRef,E+
-    free(cp_service_file);//service_file_nameもfreeされます。たぶん。
+    char *service_file_name = (char *)basename(cp_service_file);
+    service_file_name[strlen(service_file_name) - 3] = '\0';      // -3 = strlen(".py") equal to basename $1 .py
+    PyObject *pModule = PyImport_ImportModule(service_file_name); // NewRef,E+
+    free(cp_service_file);                                        // service_file_nameもfreeされます。たぶん。
     /* import文を使い、pythonファイルをモジュールとしてインポートする */
-    if (pModule == NULL) {
+    if (pModule == NULL)
+    {
         /* pythonでは、モジュールのインポートでもエラーが起こる。*/
         pyci_debug_printf(PYCI_ERROR_LEVEL, "FAIL at PyImport_Import(%s)", service_file_name);
         return NULL;
